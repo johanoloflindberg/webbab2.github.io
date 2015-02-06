a7456cf36f843403bc0b93bd9c45c8fd8a086130
Victor Schelin <victor@adaptivemedia.se>
2015-02-06 09:52:32 +0100

    Layout and resp changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-;(function(){
+(function($){
 
     // Menu settings
     $('#menu-toggle, .menu-close').on('click', function(){
@@ -6,5 +6,21 @@
         $('html').toggleClass('menu-open');
         $('.menu').toggleClass('open');
     });
+
+    // Hide menu when scrolling too far down
+    (function(){
+        var $header = $('.header'),
+            $menuToggle = $('#menu-toggle'),
+            height = $header.height();
+
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > height) {
+                $menuToggle.addClass('hidden');
+            }
+            else {
+                $menuToggle.removeClass('hidden');
+            }
+        });
+    })();
 
 })(jQuery);
